789706b1a0168d9271e1632c55c5db487059301f
steph-en-m <mstevejean@gmail.com>
2020-05-26 21:59:55 +0300

    state-machine

--- Automata/StateMachine.py
+++ Automata/StateMachine.py
@@ -2,3 +2,8 @@
 # consists of a set of states(initial and terminal state(s)),
 # a set of input and output events and a state transition function
 
+class StateMachine():
+    def __init__(self):
+        self.handlers = {}
+        self.initial_state = None
+        self.terminal_state = []
