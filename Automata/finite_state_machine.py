9157e46028cd3842c1927359d000333502f3f72c
steph-en-m <mstevejean@gmail.com>
2020-05-26 17:45:43 +0300

    automata and finite-state machines

---
+++ Automata/finite_state_machine.py
@@ -0,0 +1,4 @@
+# A Finite State Machine is an abstract machine that
+# consists of a set of states(initial and terminal state(s)),
+# a set of input and output events and a state transition function
+
