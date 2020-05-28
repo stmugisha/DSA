37216131a03d6c59fbd77affeb049977c88022af
steph-en-m <mstevejean@gmail.com>
2020-05-28 21:39:32 +0300

    state-machine

--- Automata/StateMachine.py
+++ Automata/StateMachine.py
@@ -4,6 +4,36 @@
 
 class StateMachine():
     def __init__(self):
-        self.handlers = {}
+        self.states = {}  #states
         self.initial_state = None
-        self.terminal_state = []
+        self.terminal_states = []
+
+    def add_state(self, name, state, terminal_state=False):
+        """ Add a new state to the set of states/handlers."""
+        name = name.upper()
+        self.states[name] = state
+        if terminal_state:
+            self.terminal_states.append(name)
+
+    def set_start(self, name):
+        """ Initial state setter."""
+        self.initial_state = name.upper()
+
+    def run(self, data):
+        """ Run state machine."""
+        try:
+            state = self.states[self.initial_state]
+        except:
+            raise InitializationError("initial_state must be set before run!")
+        if not self.terminal_states:
+            raise InitializationError("There must be atleast 1 terminal/end state")
+
+        while True:
+            (new_state, data) = state(data)
+            if new_state.upper() in self.terminal_states:
+                print(f"Current/Final state: {new_state}")
+                break
+            else:
+                state = self.states[new_state.upper()]
+
+
